0df71cab133062ba9f02ae536a5de37dafa4212c
sandyvidanes <crizandra.vidanes@orangeandcrane.com>
2021-03-18 20:49:28 +0800

    change port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var { buildSchema } = require('graphql');
 const fetchUrl = require("fetch").fetchUrl;
 
 const app = express()
-const port = 3000;
+const port = 3333;
 
 var schema = buildSchema(`
   type Response {
